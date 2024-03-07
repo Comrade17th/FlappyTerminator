f47457d230b6285ac69cdd979096d5d1ed2d771b
nasusfen <ravshannedonat@gmail.com>
2024-03-07 17:06:47 +0300

    Unsaved backuped, scene options

--- Assets/_Project/Sources/Game.cs
+++ Assets/_Project/Sources/Game.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     [SerializeField] private Player _player;
-    [SerializeField] private EnemySpawner _enemySpawner;
     [SerializeField] private StartScreen _startScreen;
     [SerializeField] private EndGameScreen _endGameScreen;
 
